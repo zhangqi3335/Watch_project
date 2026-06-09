d27ea17b1727b80c4999d4b56d91a3498a52d313
张奇 <1782087692@qq.com>
2026-06-09 16:22:11 +0800

    本项目采用互斥量的方案进行学习，在下个中级代码中采用其他方案

--- 04_Software/02_Homework/ADC_DMA_Arch_ope/Core/Src/freertos.c
+++ 04_Software/02_Homework/ADC_DMA_Arch_ope/Core/Src/freertos.c
@@ -25,7 +25,11 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+<<<<<<< HEAD
 
+=======
+#include "elog.h"
+>>>>>>> 7f7d728 (本项目采用互斥量的方案进行学习，在下个中级代码中采用其他方案)
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -35,7 +39,11 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+<<<<<<< HEAD
 
+=======
+//#define LOG_TAG "FREERTOS"
+>>>>>>> 7f7d728 (本项目采用互斥量的方案进行学习，在下个中级代码中采用其他方案)
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -117,6 +125,10 @@ void StartDefaultTask(void *argument)
   /* Infinite loop */
   for(;;)
   {
+<<<<<<< HEAD
+=======
+    log_i("HELLO");
+>>>>>>> 7f7d728 (本项目采用互斥量的方案进行学习，在下个中级代码中采用其他方案)
     osDelay(1);
   }
   /* USER CODE END StartDefaultTask */
